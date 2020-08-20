2aa431eb2e9f87dbfa02b85b884a7991b6c934f6
Nathan Steed <steedn@outlook.com>
2020-08-20 21:02:15 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+
+puts doc.css(".headline-260IBN").text
